43f45635de210dd11064e16fdc6ae2d3fa2fc879
Алексей Жуков <gkvalex7@gmail.com>
2021-09-25 11:43:16 +0300

    client routing set up and ready to go

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,11 @@ import React from 'react';
 import {BrowserRouter as Router} from 'react-router-dom';
 import {useRoutes} from './routes';
 import {useAuth} from './hooks/auth.hook';
-import {AuthContext} from './context/AuthContext';
 import {Navbar} from './components/Navbar';
 import {Loader} from './components/Loader';
-import 'materialize-css';
 
 function App() {
-  const {ready, token, login, logout, userId} = useAuth();
+  // const {ready, token, login, logout, userId} = useAuth();
   const isAuthenticated = true;
   const routes = useRoutes(isAuthenticated);
 
@@ -17,16 +15,12 @@ function App() {
   // }
 
   return (
-      <AuthContext.Provider value={{
-        token, login, logout, userId, isAuthenticated
-      }}>
-        <Router>
-          { isAuthenticated && <Navbar /> }
-          <div className="container">
-            {routes}
-          </div>
-        </Router>
-      </AuthContext.Provider>
+    <Router>
+      { isAuthenticated && <Navbar /> }
+      <div className="container">
+        {routes}
+      </div>
+    </Router>
   );
 }
 
